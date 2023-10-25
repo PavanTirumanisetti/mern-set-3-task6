2b47060a194fa967042bf2b17580f734a5aef705
PavanTirumanisetti <pavantirumanisetti@gmail.com>
2023-10-25 10:46:21 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import Header from './Components/Header';
+import About from './Components/About';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Header />
+      <About />
+      <Projects />
+      <Contact />
   </React.StrictMode>
 );
 
